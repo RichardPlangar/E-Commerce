40dcd8b9f9c440596e6c6cc1fd23643fd005acb2
Richard Plangar <richard.plangar@gmail.com>
2022-08-07 22:11:55 +0200

    react-router has been added

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,10 +1,17 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login/Login';
+import { NotFound } from './pages/NotFound/NotFound';
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
