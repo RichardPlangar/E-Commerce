2ff8807f11e7d388be25b1c9b2337a14432e3b56
Richard Plangar <richard.plangar@gmail.com>
2022-08-10 12:40:25 +0200

    nothing has been changed

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -3,7 +3,7 @@ import express from 'express';
 import errorHandler from './middlewares/error-handler';
 //import routes from './routes';
 
-export default function (database: any) {
+export default function (databaseConnection: any) {
   const app = express();
 
   app.use(cors());
