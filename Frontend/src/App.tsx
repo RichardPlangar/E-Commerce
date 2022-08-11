e2e164f375c10473c69f8b370e89d4e81a2e574c
Richard Plangar <richard.plangar@gmail.com>
2022-08-11 20:51:41 +0200

    header and registration page added

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login/Login';
+import Registration from './pages/Registration/Registration';
+import Header from './components/Header/Header';
 import { NotFound } from './pages/NotFound/NotFound';
 
 function App() {
@@ -8,7 +10,9 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
+          <Route path="/" element={<Header />}></Route>
           <Route path="/login" element={<Login />}></Route>
+          <Route path="/register" element={<Registration />}></Route>
           <Route path="*" element={<NotFound />}></Route>
         </Routes>
       </Router>
