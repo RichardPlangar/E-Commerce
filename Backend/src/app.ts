8ae52e6c20a39bd9faed8a2504a8ced596c37097
Richard Plangar <richard.plangar@gmail.com>
2022-08-11 20:52:53 +0200

    database connection has been added

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -3,9 +3,12 @@ import express from 'express';
 import errorHandler from './middlewares/error-handler';
 //import routes from './routes';
 
-export default function (databaseConnection: any) {
+export default function (database: any) {
   const app = express();
 
+  if (database) {
+    database.checkConnection();
+  }
   app.use(cors());
   app.use(express.json());
   app.use(errorHandler);
