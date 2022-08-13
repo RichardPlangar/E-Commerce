b682816d5a0080e58ef2ef331f74267332f62870
Richard Plangar <richard.plangar@gmail.com>
2022-08-13 21:56:27 +0200

    structural changes

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,16 +1,17 @@
 import cors from 'cors';
 import express from 'express';
 import errorHandler from './middlewares/error-handler';
-//import routes from './routes';
+import routes from './routes';
 
 export default function (database: any) {
   const app = express();
+  app.use(express.json());
 
   if (database) {
     database.checkConnection();
   }
+  app.use(routes());
   app.use(cors());
-  app.use(express.json());
   app.use(errorHandler);
 
   return app;
