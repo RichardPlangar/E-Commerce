6966b871ec4f92bd51da27130df61b3767035445
Richard Plangar <richard.plangar@gmail.com>
2022-09-15 10:35:25 +0200

    products component has been added to the app

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login/Login';
 import Registration from './pages/Registration/Registration';
+import Products from './pages/Main/Main';
 import Header from './components/Header/Header';
 import { NotFound } from './pages/NotFound/NotFound';
 
@@ -9,8 +10,9 @@ function App() {
   return (
     <div className="App">
       <Router>
+        <Header></Header>
         <Routes>
-          <Route path="/" element={<Header />}></Route>
+          <Route path="/" element={<Products />}></Route>
           <Route path="/login" element={<Login />}></Route>
           <Route path="/register" element={<Registration />}></Route>
           <Route path="*" element={<NotFound />}></Route>
