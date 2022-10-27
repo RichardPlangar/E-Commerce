68c3a87f487683551199f35e187a36dc782fc152
Richard Plangar <richard.plangar@gmail.com>
2022-10-27 13:36:07 +0200

    get category endpoint has been added to routes

--- Backend/src/routes/index.ts
+++ Backend/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import user from './user';
+import category from './category';
 
 export default () => {
   const appRouter = Router();
   user(appRouter);
+  category(appRouter);
   return appRouter;
 };
