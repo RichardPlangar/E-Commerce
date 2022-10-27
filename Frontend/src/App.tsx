62f9cba87a3a3f7a760c05944b60647c029bbc10
Richard Plangar <richard.plangar@gmail.com>
2022-10-27 13:39:28 +0200

    Categories component has been added to the page

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -4,6 +4,7 @@ import Login from './pages/Login/Login';
 import Registration from './pages/Registration/Registration';
 import Products from './pages/Main/Main';
 import Header from './components/Header/Header';
+import Categories from './components/Categories/Categories';
 import { NotFound } from './pages/NotFound/NotFound';
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
           <Route path="/register" element={<Registration />}></Route>
           <Route path="*" element={<NotFound />}></Route>
         </Routes>
+        <Categories />
       </Router>
     </div>
   );
