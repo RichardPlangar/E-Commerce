16ea6119064c0b3bf574375656b20577d2dce0ca
Richard Plangar <richard.plangar@gmail.com>
2022-10-27 13:38:58 +0200

    dummy text for test purposes

---
+++ Frontend/src/pages/Main/Main.tsx
@@ -0,0 +1,13 @@
+import './Main.scss';
+
+const Main: React.FC = () => {
+  return (
+    <div className="product-main-container">
+      <div className="main-text">
+        <h1>We don't advertise!</h1>
+        <h2>Pick and go!</h2>
+      </div>
+    </div>
+  );
+};
+export default Main;
