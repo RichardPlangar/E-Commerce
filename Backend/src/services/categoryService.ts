11b24bf2d7a76a0340564a314f969e96d01d22b5
Richard Plangar <richard.plangar@gmail.com>
2022-10-30 19:29:50 +0100

    error handling has been added when the table is empty

--- Backend/src/services/categoryService.ts
+++ Backend/src/services/categoryService.ts
@@ -2,9 +2,15 @@ import { categoryRepository } from '../repositories/categoryRepository';
 import { CategoryDomainModel } from '../models/domain/CategoryDomainModel';
 
 const categoryService = {
-  async getCategory(): Promise<CategoryDomainModel[]> {
-    const allCategory = await categoryRepository.getCategory();
-    return allCategory;
+  async getCategory(): Promise<string[]> {
+    const allCategoryFromDatabase: CategoryDomainModel[] =
+      await categoryRepository.getCategory();
+
+    if (allCategoryFromDatabase.length) {
+      const allCategory = allCategoryFromDatabase.map((x) => x.category);
+      return allCategory;
+    }
+    throw new Error('There are no categories available!');
   },
 };
 
