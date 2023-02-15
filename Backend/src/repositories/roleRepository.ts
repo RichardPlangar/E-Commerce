fada67c2ca1661a5470612d5fdf3aa2e9ffda966
Richard Plangar <richard.plangar@gmail.com>
2023-02-15 10:09:27 +0100

    roleRepository has been created

---
+++ Backend/src/repositories/roleRepository.ts
@@ -0,0 +1,10 @@
+import { RoleDomainModel } from '../models/domain/RoleDomainModel';
+
+export const roleRepository = {
+  findRoleById(id: number): RoleDomainModel {
+    return {
+      id,
+      name: 'Customer',
+    };
+  },
+};
